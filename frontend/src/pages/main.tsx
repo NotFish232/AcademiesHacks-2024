4aab5290739b05d30348d55106aca5134a22f430
ryanghimire <2025sghimire@tjhsst.edu>
2024-05-18 13:43:45 -0400

    shad

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,5 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
 function MainPage() {
-    return <div>Main Page</div>;
+    return (
+        <div>
+            <Button>Click me</Button>
+        </div>
+    )
 }
 
 export default MainPage;
