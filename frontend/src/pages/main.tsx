961c995effc2e88f30e78f18452b3d0718872920
ryanghimire <2025sghimire@tjhsst.edu>
2024-05-18 16:21:36 -0400

    done

--- frontend/src/pages/main.tsx
+++ frontend/src/pages/main.tsx
@@ -1,11 +1,38 @@
-import { Button } from "@/components/ui/button"
+import { About } from "@/components/otherstuff/About";
+import { Cta } from "@/components/otherstuff/Cta";
+import { FAQ } from "@/components/otherstuff/FAQ";
+import { Features } from "@/components/otherstuff/Features";
+import { Footer } from "@/components/otherstuff/Footer";
+import { Hero } from "@/components/otherstuff/Hero";
+import { HowItWorks } from "@/components/otherstuff/HowItWorks";
+import { Navbar } from "@/components/otherstuff/Navbar";
+import { Newsletter } from "@/components/otherstuff/Newsletter";
+import { Pricing } from "@/components/otherstuff/Pricing";
+import { ScrollToTop } from "@/components/otherstuff/ScrollToTop";
+import { Services } from "@/components/otherstuff/Services";
+import { Sponsors } from "@/components/otherstuff/Sponsors";
+import { Team } from "@/components/otherstuff/Team";
+import { Testimonials } from "@/components/otherstuff/Testimonials";
 
-function MainPage() {
-    return (
-        <div>
-            <Button>Click me</Button>
-        </div>
-    )
+export default function MainPage() {
+  return (
+    <>
+      <p>Testing</p>
+        <Navbar />
+        <Hero />
+        <Sponsors />
+        <About />
+        <HowItWorks />
+        <Features />
+        <Services />
+        <Cta />
+        <Testimonials />
+        <Team />
+        <Pricing />
+        <Newsletter />
+        <FAQ />
+        <Footer />
+        <ScrollToTop />
+    </>
+  )
 }
-
-export default MainPage;
