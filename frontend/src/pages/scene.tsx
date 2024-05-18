1a97b5c463315e034d503369193283027c9b3101
Justin Lee <2025jlee@tjhsst.edu>
2024-05-18 13:22:29 -0400

    start scene work

---
+++ frontend/src/pages/scene.tsx
@@ -0,0 +1,29 @@
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { Suspense, useRef, useState } from 'react';
+import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+
+
+
+function ScenePage() {
+    const gltf_ref = useRef();
+    const gltf = useLoader(GLTFLoader, "/test.gltf");
+
+    return (
+        <Canvas className='w-40 h-full'>
+            <ambientLight intensity={0.25} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+
+            <primitive
+                ref={gltf_ref}
+                object={gltf.scene}
+                position={[0, 0, 0]}
+            />
+            <OrbitControls />
+
+        </Canvas>
+    );
+}
+
+export default ScenePage;
