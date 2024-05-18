9eb916893adfa3c8cfa8c889ede356a81ee61385
Justin Lee <2025jlee@tjhsst.edu>
2024-05-18 17:02:09 -0400

    sel

--- frontend/src/pages/select.tsx
+++ frontend/src/pages/select.tsx
@@ -1,5 +1,29 @@
+import { backend_url } from "@/shared";
+import { useEffect, useState } from "react";
+
+function useScenes() {
+    const [scenes, setScenes] = useState<string[] | null>(null);
+
+    useEffect(() => {
+        fetch(`${backend_url}/scene/list`).then(r => r.json()).then(setScenes);
+    }, []);
+
+    return scenes;
+}
+
+interface SceneComponentProps {
+    scene: string;
+    onClick: (scene: string) => void;
+}
+
+function SceneComponent(props: SceneComponentProps) {
+    return <div onClick={() => props.onClick(props.scene)}>props.scene</div>;
+}
+
 function SelectPage() {
-    return <div>Select Page</div>;
+    const scenes = useScenes();
+    const [selectedScene, setSelectedScene] = useState<string | null>(null);
+    return <div>{scenes?.map(s => <SceneComponent scene={s} onClick={(s) => setSelectedScene(s)} />)} </div>;
 }
 
 export default SelectPage;
